7dcb5e9f9883059f0af090530f183fb1c0f0a6fd
theWebCookie <jabwojtek@wp.pl>
2024-01-02 20:44:25 +0100

    feat: add car repo

--- CarRent.Api/Endpoints/CarsEndpoints.cs
+++ CarRent.Api/Endpoints/CarsEndpoints.cs
@@ -1,85 +1,35 @@
 using CarRent.Api.Entities;
+using CarRent.Api.Repositories;
 
 namespace CarRent.Api.Endpoints;
 
 public static class CarsEndpoints
 {
   const string GetCarEndpointName = "GetCarById";
-  static List<Car> cars = new()
-  {
-    new Car()
-    {
-      Id = 1,
-      Title = "BMW",
-      ImageUri = "https://placehold.co/100",
-      Fuel = 5.2,
-      Luggage = 30,
-      Doors = 5,
-      Seats = 5,
-      FuelUsage = 6.7,
-      CarType = "car",
-      Transmission = "automat",
-      Description = "aaaaaaaaaaaaaaaaaaaaaaaaa",
-      Price = 200,
-    },
-      new Car()
-    {
-      Id = 2,
-      Title = "AUDI",
-      ImageUri = "https://placehold.co/100",
-      Fuel = 6.2,
-      Luggage = 35,
-      Doors = 3,
-      Seats = 2,
-      FuelUsage = 12.7,
-      CarType = "car",
-      Transmission = "automat",
-      Description = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-      Price = 500,
-    },
-      new Car()
-    {
-      Id = 3,
-      Title = "Mercedes",
-      ImageUri = "https://placehold.co/100",
-      Fuel = 3.2,
-      Luggage = 20,
-      Doors = 3,
-      Seats = 4,
-      FuelUsage = 5.7,
-      CarType = "car",
-      Transmission = "automat",
-      Description = "ccccccccccccccccccccccccccccc",
-      Price = 150,
-    }
-  };
   public static RouteGroupBuilder MapCarsEndpoints(this IEndpointRouteBuilder routes)
   {
+    InMemCarsRepository repository = new();
+
     var group = routes.MapGroup("/cars").WithParameterValidation();
 
-    group.MapGet("/", () => cars);
+    group.MapGet("/", () => repository.GetAll());
 
     group.MapGet("/{id}", (int id) =>
     {
-      Car? car = cars.Find(car => car.Id == id);
-      if (car is null)
-      {
-        return Results.NotFound();
-      }
-      return Results.Ok(car);
+      Car? car = repository.Get(id);
+      return car is not null ? Results.Ok(car) : Results.NotFound();
     })
     .WithName(GetCarEndpointName);
 
     group.MapPost("/", (Car car) =>
     {
-      car.Id = cars.Max(car => car.Id) + 1;
-      cars.Add(car);
+      repository.Create(car);
       return Results.CreatedAtRoute(GetCarEndpointName, new { id = car.Id }, car);
     });
 
     group.MapPut("/{id}", (int id, Car updatedCar) =>
     {
-      Car? existingCar = cars.Find(car => car.Id == id);
+      Car? existingCar = repository.Get(id);
       if (existingCar is null)
       {
         return Results.NotFound();
@@ -97,15 +47,16 @@ public static class CarsEndpoints
       existingCar.Description = updatedCar.Description;
       existingCar.Price = updatedCar.Price;
 
+      repository.Update(existingCar);
       return Results.NoContent();
     });
 
     group.MapDelete("/{id}", (int id) =>
     {
-      Car? car = cars.Find(car => car.Id == id);
+      Car? car = repository.Get(id);
       if (car is not null)
       {
-        cars.Remove(car);
+        repository.Delete(id);
       }
 
       return Results.NoContent();
---
+++ CarRent.Api/Repositories/InMemCarsRepository.cs
@@ -0,0 +1,84 @@
+using CarRent.Api.Entities;
+
+namespace CarRent.Api.Repositories
+{
+  public class InMemCarsRepository
+  {
+    private readonly List<Car> cars = new()
+    {
+      new Car()
+      {
+        Id = 1,
+        Title = "BMW",
+        ImageUri = "https://placehold.co/100",
+        Fuel = 5.2,
+        Luggage = 30,
+        Doors = 5,
+        Seats = 5,
+        FuelUsage = 6.7,
+        CarType = "car",
+        Transmission = "automat",
+        Description = "aaaaaaaaaaaaaaaaaaaaaaaaa",
+        Price = 200,
+      },
+        new Car()
+      {
+        Id = 2,
+        Title = "AUDI",
+        ImageUri = "https://placehold.co/100",
+        Fuel = 6.2,
+        Luggage = 35,
+        Doors = 3,
+        Seats = 2,
+        FuelUsage = 12.7,
+        CarType = "car",
+        Transmission = "automat",
+        Description = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+        Price = 500,
+      },
+        new Car()
+      {
+        Id = 3,
+        Title = "Mercedes",
+        ImageUri = "https://placehold.co/100",
+        Fuel = 3.2,
+        Luggage = 20,
+        Doors = 3,
+        Seats = 4,
+        FuelUsage = 5.7,
+        CarType = "car",
+        Transmission = "automat",
+        Description = "ccccccccccccccccccccccccccccc",
+        Price = 150,
+      }
+    };
+
+    public IEnumerable<Car> GetAll()
+    {
+      return cars;
+    }
+
+    public Car? Get(int id)
+    {
+      return cars.Find(car => car.Id == id);
+    }
+
+    public void Create(Car car)
+    {
+      car.Id = cars.Max(car => car.Id) + 1;
+      cars.Add(car);
+    }
+
+    public void Update(Car updatedCar)
+    {
+      var index = cars.FindIndex(car => car.Id == updatedCar.Id);
+      cars[index] = updatedCar;
+    }
+
+    public void Delete(int id)
+    {
+      var index = cars.FindIndex(car => car.Id == id);
+      cars.RemoveAt(index);
+    }
+  }
+}
