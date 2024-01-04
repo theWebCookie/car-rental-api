17a7291897c6c1a450279da0ee8567478a69ec2a
theWebCookie <jabwojtek@wp.pl>
2024-01-04 19:35:44 +0100

    feat: use ef car repository

--- CarRent.Api/Data/DataExtensions.cs
+++ CarRent.Api/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using CarRent.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRent.Api.Data;
@@ -9,5 +10,12 @@ public static class DataExtensions
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<CarRentContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connString = configuration.GetConnectionString("CrContext");
+        services.AddSqlServer<CarRentContext>(connString).AddScoped<ICarsRepository, EntityFrameworkCarsRepository>();
+        return services;
     }
 }
--- CarRent.Api/Program.cs
+++ CarRent.Api/Program.cs
@@ -1,13 +1,8 @@
 using CarRent.Api.Data;
 using CarRent.Api.Endpoints;
-using CarRent.Api.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<ICarsRepository, InMemCarsRepository>();
-
-var connString = builder.Configuration.GetConnectionString("CrContext");
-builder.Services.AddSqlServer<CarRentContext>(connString);
+builder.Services.AddRepositories(builder.Configuration);
 
 var app = builder.Build();
 
